32aedead7cbc6c75c8b011c7fbcf951ef0cdd330
sudexp <sudexp@icloud.com>
2019-09-17 17:01:00 +0300

    first app created

---
+++ firstApplication/app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.firstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun handleClick(view: View) {
+        textView.text = getString(R.string.clicked_text)
+    }
+}
