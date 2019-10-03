7c6ceb7704f4eaa9060ea1a44c64e9c5aeb2b173
sudexp <sudexp@icloud.com>
2019-10-03 10:39:42 +0300

    GoogleMapsExample app created

---
+++ GoogleMapsExample/app/src/main/java/com/example/googlemapsexample/MapsActivity.kt
@@ -0,0 +1,54 @@
+package com.example.googlemapsexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        // val sydney = LatLng(-34.0, 151.0)
+        // mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        val jyvaskyla = LatLng(62.242561, 25.747499)
+        val options = MarkerOptions().position(jyvaskyla).title("Jyväskylä")
+
+
+        val dynamo = LatLng(62.2416223, 25.7597309)
+        mMap.addMarker(MarkerOptions().position(dynamo).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dynamo, 14.0F))
+
+        mMap.addMarker(options)
+        mMap.uiSettings.isZoomControlsEnabled = true
+    }
+}
